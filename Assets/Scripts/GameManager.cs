0c219d47c981c6498f10c436a13fff86ed3a122b
Lenny066 <lennylimonoff@gmail.com>
2026-06-01 11:43:18 -0300

    OK

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float timer = 60f;
+    private UIManager uiManager;
+
+    void Start()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            uiManager.UpdateTimer(timer);
+        }
+        else
+        {
+            timer = 0;
+        }
+    }
+}
