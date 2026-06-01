34650ba18e1e2a25e48ccc7142dd14dae7a9d3ca
Lenny066 <lennylimonoff@gmail.com>
2026-06-01 12:09:55 -0300

    OK

---
+++ Assets/Scripts/InteractiveArea.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class InteractiveArea : MonoBehaviour
+{
+    private int score = 0; 
+    private UIManager uiManager; 
+
+    private void Awake()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+
+        if (uiManager == null)
+        {
+            Debug.LogError("¡Atención! No se encontró el UIManager en la escena.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+  
+        if (other.CompareTag("Coleccionable"))
+        {
+            score++; 
+
+            if (uiManager != null)
+            {
+                uiManager.UpdateScore(score); 
+            }
+
+            Destroy(other.gameObject); 
+        }
+    }
+}
